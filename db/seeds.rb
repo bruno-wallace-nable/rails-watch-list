fd78a376bb6743981fe46130dd9934cf7331dc0b
Bruno Wallace <brunownm@gmail.com>
2026-03-10 14:12:57 -0300

    first app commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "json"
+require "open-uri"
+
+puts "Cleaning database..."
+Bookmark.destroy_all
+Movie.destroy_all
+
+puts "Creating movies from API..."
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+response = URI.open(url).read
+movies = JSON.parse(response)["results"]
+
+movies.each do |movie|
+  puts "Creating #{movie['title']}"
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "Finished! Created #{Movie.count} movies."
